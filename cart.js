7706243d19ce3ddb226d39f04f5af65d85baf192
Hamzamt99 <tamarihamza4@gmail.com>
2021-08-20 18:17:26 +0300

    Lab11 done

---
+++ cart.js
@@ -0,0 +1,72 @@
+/* global Cart */
+'use strict';
+
+// Create an event listener so that when the delete link is clicked, the removeItemFromCart method is invoked.
+const table = document.getElementById('cart');
+table.addEventListener('click', removeItemFromCart);
+let cart;
+
+
+function loadCart() {
+  const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+  cart = new Cart(cartItems);
+  console.log(cart);
+}
+
+// Make magic happen --- re-pull the Cart, clear out the screen and re-draw it
+function renderCart() {
+  loadCart();
+  clearCart();
+  showCart();
+}
+
+// TODO: Remove all of the rows (tr) in the cart table (tbody)
+function clearCart() {
+  let tbodyEl = document.getElementsByTagName('tbody')[0];
+}
+
+// TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
+function showCart() {
+  // TODO: Find the table body
+  let tbodyEl = document.getElementsByTagName('tbody')[0];
+  
+  // TODO: Iterate over the items in the cart
+  // TODO: Create a TR
+  // TODO: Create a TD for the delete link, quantity,  and the item
+  // TODO: Add the TR to the TBODY and each of the TD's to the TR
+  
+  for (let i = 0; i < cart.items.length; i++) {
+
+    let trEl = document.createElement('tr');
+    
+    let deleteLink = document.createElement('td');
+    deleteLink.innerHTML = '<a>X</a>';
+    trEl.appendChild(deleteLink);
+    deleteLink.setAttribute('del', );
+
+    let quantityTd = document.createElement('td');
+    quantityTd.textContent = cart.items[i].quantity;
+    trEl.appendChild(quantityTd);
+
+    let itemTd = document.createElement('td');
+    itemTd.textContent = cart.items[i].product;
+    trEl.appendChild(itemTd);
+
+    tbodyEl.appendChild(trEl);
+  }
+
+}
+
+function removeItemFromCart(event) {
+
+  // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
+  // TODO: Save the cart back to local storage
+  // TODO: Re-draw the cart table
+  for (let i = 0; i < array.length; i++) {
+    
+    
+  }
+}
+
+// This will initialize the page and draw the cart on screen
+renderCart();
